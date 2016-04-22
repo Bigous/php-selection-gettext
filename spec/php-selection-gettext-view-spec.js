315913aefd15cfcf65655e36638ef9348131266c
Richard Natal <bigous@gmail.com>
2016-04-22 09:44:16 -0300

    First commit

---
+++ spec/php-selection-gettext-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PhpSelectionGettextView from '../lib/php-selection-gettext-view';
+
+describe('PhpSelectionGettextView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
